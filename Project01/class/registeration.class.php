4576e842c9fb7f1398591806c28e36f1e9ace46a
Edward Riley <55459510+emr9018@users.noreply.github.com>
2020-02-27 16:51:10 -0500

    Registration complete

--- Project01/class/registeration.class.php
+++ Project01/class/registeration.class.php
@@ -15,11 +15,33 @@ class DB
 
         if ($this->conn->connect_error)
         {
-            echo "Connection Failed: " . mysqli_connect_error();
+            echo "Connection Failed: " . mysqli_connect_error(); 
             die();
         } 
         
     
     } // constructor
+
+    function insertRow($name, $password, $role)
+    {
+        
+        $this->establishConnection();
+
+        $stmt = $this->conn->prepare("INSERT INTO attendee (name, password, role) VALUES (?, ?, ?)");
+        $stmt->bind_param("ssi", $name, $password, $role);
+
+        $stmt->execute();
+      
+        echo "New records created successfully";
+
+        $stmt->close();
+        $this->conn->close();    
+    }
+
+    function checkLogin()
+    {
+        
+    }
+
 }
 ?>
--- Project01/register.php
+++ Project01/register.php
@@ -5,15 +5,103 @@ $db->establishConnection();
 
 
 echo "<h1>Registration Page</h1>";
-echo "<form>";
+echo "<form method='POST'>";
 echo "<p> Username: <input type='text' id='register_name' name='register_name'></p>";
 echo "<p> Password: <input type='password' id='register_password' name='register_password'></p>";
 echo "<p> Password Verification: <input type='password' id='verification_register_password' name='verification_register_password'></p>";
-echo "<input type='submit' value='Register'>";
+echo "<p>Role:  <select id='register_role' name='register_role'>
+<option value='administrator'>Administrator</option>
+<option value='eventmanager'>Event Manager</option>
+<option value='attendee'>Attendee</option>
+</select></p>";
+echo "<input type='submit' name='submitName' value='Register'>";
 echo "</form>";
 
+echo "<p>";
+if (isset($_GET['register_name']))
+{
+    if ($_GET['register_name'] == "")
+    {
+        echo "Please enter a valid username.";
+    }
+    else 
+    {
+        echo $_GET['register_name'];
+    }
+}
 
+echo "</p>";
 
+echo "<p>";
 
+if (isset($_GET['register_password']))
+{
+    if ($_GET['register_password'] == "")
+    {
+        echo "Please enter a valid password.";
+    }
+    else 
+    {
+        echo $_GET['register_password'];
+    }
+}
+echo "</p>";
+
+echo "<p>";
+
+if (isset($_GET['verification_register_password']))
+{
+    if ($_GET['verification_register_password'] == "")
+    {
+        echo "Please enter a verification password.";
+    }
+    else if ($_GET['verification_register_password'] != $_GET['register_password']) 
+    {
+        echo "Passwords do not match!";
+    }
+    else 
+    {
+        echo $_GET['verification_register_password'];
+    }
+}
+echo "</p>";
+echo "<p>";
+
+if (isset($_GET['register_role']))
+{
+        echo $_GET['register_role'];
+}
+echo "</p>";
+
+if (isset($_POST['submitName']))
+{
+    // role to numbers
+    if ($_POST['register_role'] == "administrator")
+    {
+        $role = 1;
+    }
+    else if ($_POST['register_role'] == "eventmanager")
+    {
+        $role = 2;
+    }
+    else if ($_POST['register_role'] == "attendee")
+    {
+        $role = 3;
+    }
+    else 
+    {
+        $role = -1;
+    }
+
+    // insert row
+    try 
+    {    
+        $db->insertRow($_POST['register_name'], $_POST['register_password'], $role);
+    }
+    catch (Exception $e)
+    {
+        echo "<p> $e </p>";
+    }
+}
 
 ?>
