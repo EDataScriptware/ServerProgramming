37b52b1fa64ecf8b0ad1e40dd02d141529c94d03
Edward Riley <55459510+emr9018@users.noreply.github.com>
2020-03-28 20:43:37 -0500

    Finished REST Lab

--- Online01_RESTful_CSharp/ProductService/Controllers/ProductController.cs
+++ Online01_RESTful_CSharp/ProductService/Controllers/ProductController.cs
@@ -9,6 +9,7 @@ namespace ProductService.Controllers
 
     public class ProductController : Controller
     {
+
         private List<Product> products = new List<Product>();
 
         public ProductController()
@@ -18,18 +19,104 @@ namespace ProductService.Controllers
 
         private void CreateProducts()
         {
-            for (int i = 1; i <= 5; i++)
-            {
-                products.Add(new Product(){Name = "Product"+i, Id=i});
-            }
+            // for (int i = 1; i <= 5; i++)
+            // {
+            //     products.Add(new Product(){Name = "Product"+i, Id=i});
+            // }
+            products.Add(new Product(){Name = "Apples", Id=1, Cost=3.99M});
+            products.Add(new Product(){Name = "Peaches", Id=2, Cost=4.05M});
+            products.Add(new Product(){Name = "Pumpkin", Id=3, Cost=13.99M});
+            products.Add(new Product(){Name = "Apples", Id=4, Cost=8.00M});
+
         }
 
-        [HttpGet]
+       /* [HttpGet]
         public IEnumerable<Product> GetAll()
         {
             return this.products;
         }
+*/
 
+        [HttpGet(Name="getNames")]
+        public string getNames(long id, string code)
+        {
+            string result = "";
+            for (int i = 0; products.Capacity > i; i++)
+            {
+                result += products[i].Name + " has a cost of $" + products[i].Cost + "\n";
+            }
+
+            return (result);
+        }
+  
+        [HttpGet("Cheapest", Name="getCheapest")]
+        public string getCheapest(long id, string code)
+        {
+            string result = "";
+
+            int targetID = products[0].Id;
+            string cheapestName = products[0].Name;
+            decimal cheapestCost = products[0].Cost; 
+
+            foreach(Product product in products)
+            {
+                if(product.Cost <= cheapestCost)
+                {
+                    cheapestCost = product.Cost;
+                    targetID = product.Id;
+                    cheapestName = product.Name;
+                }
+            }
+
+            result = cheapestName + " is the cheapest with the cost of $" + cheapestCost;
+            return (result);
+        }
+
+        [HttpGet("Costliest", Name="getCostliest")]
+        public string getCostliest(long id, string code)
+        {
+            string result = "";
+            int targetID = products[0].Id;
+            string expensiveName = products[0].Name;
+            decimal expensiveCost = products[0].Cost; 
+
+            foreach(Product product in products)
+            {
+                if(product.Cost >= expensiveCost)
+                {
+                    expensiveCost = product.Cost;
+                    targetID = product.Id;
+                    expensiveName = product.Name;
+                }
+            }
+
+            result = expensiveName + " is the costliest with the cost of $" + expensiveCost;
+            return (result);
+        }
+        
+        [HttpGet("{productName}", Name="getPrice")]
+        public string getPrice(string productName, string code)
+        {
+
+            decimal foundCost = 0.00M;
+            string result;
+            foreach(Product productList in products)
+            {
+                if (productName.ToLower() == productList.Name.ToLower())
+                {
+                    foundCost = productList.Cost;
+                    result = "$" + foundCost + " is the cost for " + productName;
+                    return (result);
+                }
+            }
+            return ("Error! Unable to find the cost for " + productName + "!");
+
+
+        }
+
+
+        /////////
+        /*
         [HttpGet("{id}", Name="GetProduct")]
         public IActionResult GetById(long id)
         {
@@ -91,6 +178,11 @@ namespace ProductService.Controllers
             return ("id: " + id + "c = " + code);
     
         }
+
+
+      
+
+       */
 
     } // class
 
--- Online01_RESTful_CSharp/ProductService/Models/Product.cs
+++ Online01_RESTful_CSharp/ProductService/Models/Product.cs
@@ -4,9 +4,10 @@ namespace ProductService.Models
     {
         public int Id {get; set;}
         public string Name {get; set;}
+        public decimal Cost {get; set;}
         public override string ToString()
         {
-            return "ID: " + Id + "Name:" + Name ;
+            return "ID: " + Id + "Name:" + Name + "Cost:" + Cost;
         }
     }
 }
