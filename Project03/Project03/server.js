414de371fb4c69e5e4a7d670e4b4578e51f40a2e
Edward Riley <55459510+emr9018@users.noreply.github.com>
2020-04-25 21:04:39 -0500

    Packages

--- Project03/Project03/server.js
+++ Project03/Project03/server.js
@@ -1,3 +1,28 @@
 var DataLayer = require("./companydata/index.js");
+var express = require('express');
+var cookieParser = require('cookie-parser');
+ 
 var dl = new DataLayer("emr9018");
+var app = express();
+app.use(cookieParser())
 
+// Needed for POST method
+var urlencodedParser = express.urlencoded({extended:false});
+
+app.use(express.static('companydata'));
+
+var server = app.listen(8080, function()
+{
+    var host = server.address().address;
+    var port = server.address().port;
+    
+    console.log("Listening at http://" + host + ":" + port);
+
+})
+
+app.get('/', function(req, res)
+{
+    console.log("Got a GET request for the root page!");
+
+    
+});
